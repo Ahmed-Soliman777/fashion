f3ebf381f80972fbc372e4151406b3a307cf1a26
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-09-09 23:49:11 +0300

    create section header component

---
+++ components/ui/SectionHeader.tsx
@@ -0,0 +1,24 @@
+interface SectionHeaderProps {
+    title: string,
+    subTitle: string
+}
+const SectionHeader = ({
+    title,
+    subTitle
+}: SectionHeaderProps) => {
+    return (
+        <div className="mx-auto text-center max-w-2xl py-8">
+
+            <h2 className="text-3xl font-bold tracking-tight text-foreground sm:text-4xl">
+                {title}
+            </h2>
+
+            <p className="mt-4 text-base leading-7 text-muted-foreground">
+                {subTitle}
+            </p>
+
+        </div>
+    )
+}
+
+export default SectionHeader
