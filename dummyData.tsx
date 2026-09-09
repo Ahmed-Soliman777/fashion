2a62f69267d1d6ae71544708a47c38f60a935c10
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-09-09 23:47:40 +0300

    create dummy data file

---
+++ dummyData.tsx
@@ -0,0 +1,32 @@
+export const dummyLatestCollections = [
+    {
+        id: 1,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 2,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 3,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 4,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 5,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+]
