becb5c68d324debfce50d43aab705bb85c5d2b0b
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-09-13 00:24:29 +0300

    create shop page

---
+++ app/shop/page.tsx
@@ -0,0 +1,43 @@
+import FrontendLayout from '@/components/layouts/FrontendLayout'
+import ProductCard from '@/components/products/ProductCard'
+import FilterOptions from '@/components/shop/FilterOptions'
+import { dummyShopProducts } from '@/dummyData'
+import React from 'react'
+
+const page = () => {
+    return (
+        <FrontendLayout>
+            <p className="flex flex-col sm:flex-row gap-5 my-10">
+                <FilterOptions />
+            </p>
+            <div className="flex-1">
+
+                <div className="flex justify-between items-center text-base sm:text-2xl mb-4">
+                    <h2 className="text-primary font-semibold">Shop</h2>
+                    <select className="border border-border text-sm p-3">
+                        <option value="low-high">
+                            Sort By: Low to High
+                        </option>
+                        <option value="high-low">
+                            Sort By: Low to High
+                        </option>
+                    </select>
+                </div>
+
+                <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+                    {
+                        dummyShopProducts.map((product) => (
+                            <ProductCard
+                                key={product.id}
+                                product={product}
+                            />
+                        ))
+                    }
+                </div>
+
+            </div>
+        </FrontendLayout>
+    )
+}
+
+export default page
--- dummyData.tsx
+++ dummyData.tsx
@@ -63,3 +63,66 @@ export const dummyBestSellers = [
         price: 79.99
     },
 ]
+
+export const dummyShopProducts = [
+    {
+        id: 1,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 2,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 3,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 4,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 5,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 6,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 7,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 8,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 9,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+    {
+        id: 10,
+        name: "Classic Jacket",
+        image: "/product1.png",
+        price: 79.99
+    },
+]
