da6b507746da11fc783a352a2976d2f3d1eca689
Ахмед Сулиман <115305520+AhmdSolimn777@users.noreply.github.com>
2026-09-14 00:08:53 +0300

    refactor: fix shop layout

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -7,34 +7,34 @@ import React from 'react'
 const page = () => {
     return (
         <FrontendLayout>
-            <p className="flex flex-col sm:flex-row gap-5 my-10">
+            <div className="flex flex-col sm:flex-row gap-5 my-10">
                 <FilterOptions />
-            </p>
-            <div className="flex-1">
+                <div className="flex-1">
 
-                <div className="flex justify-between items-center text-base sm:text-2xl mb-4">
-                    <h2 className="text-primary font-semibold">Shop</h2>
-                    <select className="border border-border text-sm p-3">
-                        <option value="low-high">
-                            Sort By: Low to High
-                        </option>
-                        <option value="high-low">
-                            Sort By: Low to High
-                        </option>
-                    </select>
+                    <div className="flex justify-between items-center text-base sm:text-2xl mb-4">
+                        <h2 className="text-primary font-semibold">Shop</h2>
+                        <select className="border border-border text-sm p-3">
+                            <option value="low-high">
+                                Sort By: Low to High
+                            </option>
+                            <option value="high-low">
+                                Sort By: Low to High
+                            </option>
+                        </select>
+                    </div>
+
+                    <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
+                        {
+                            dummyShopProducts.map((product) => (
+                                <ProductCard
+                                    key={product.id}
+                                    product={product}
+                                />
+                            ))
+                        }
+                    </div>
+
                 </div>
-
-                <div className="grid grid-cols-2 gap-6 md:grid-cols-3 lg:grid-cols-4">
-                    {
-                        dummyShopProducts.map((product) => (
-                            <ProductCard
-                                key={product.id}
-                                product={product}
-                            />
-                        ))
-                    }
-                </div>
-
             </div>
         </FrontendLayout>
     )
